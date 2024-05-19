65990ac3040eab60036ed0aa597339f177eae032
Reda Lemeden <git@redalemeden.com>
2024-05-19 02:52:04 +0200

    Use custom alert

--- src/components/ComboMaker.svelte
+++ src/components/ComboMaker.svelte
@@ -26,6 +26,12 @@
     sequence: [],
   };
 
+  let alertText = "";
+
+  function closeAlert() {
+    alertText = "";
+  }
+
   onMount(() => {
     const urlParams = new URLSearchParams(window.location.search);
     const comboQuery = urlParams.get("combo");
@@ -89,11 +95,16 @@
 
     navigator.clipboard.writeText(text);
 
-    if (selectedTextMode === 1) {
-      window.location.href = text;
-    }
-
-    alert(`Copied to clipboard!\n\n${text}`);
+    alertText =
+      selectedTextMode !== 1
+        ? text
+        : "A public URL to view this combo has been generated.";
+    setTimeout(() => {
+      alertText = "";
+      if (selectedTextMode === 1) {
+        window.location.href = text;
+      }
+    }, 5000);
   }
 
   function copyAsImage() {
@@ -427,5 +438,64 @@
     >
       Save as <span class="text-lime-400">image</span>
     </button>
+  </div>
+</div>
+
+<div
+  aria-live="assertive"
+  class="pointer-events-none fixed inset-0 flex items-end px-4 py-6 sm:items-start sm:p-6"
+>
+  <div class="flex w-full flex-col items-center space-y-4 sm:items-end">
+    {#if alertText !== ""}
+      <div
+        class="pointer-events-auto w-full max-w-sm overflow-hidden rounded-lg bg-gray-900 shadow-lg ring-1 ring-black ring-opacity-5"
+      >
+        <div class="p-4">
+          <div class="flex items-start">
+            <div class="flex-shrink-0">
+              <svg
+                class="h-6 w-6 text-lime-500"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="currentColor"
+                aria-hidden="true"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+                />
+              </svg>
+            </div>
+            <div class="ml-3 w-0 flex-1 pt-0.5 font-mono">
+              <p class="text-sm font-medium">Combo copied!</p>
+              <p class="mt-1 text-sm text-gray-400">
+                {alertText}
+              </p>
+            </div>
+            <div class="ml-4 flex flex-shrink-0">
+              <button
+                type="button"
+                class="inline-flex rounded-md bg-black text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+                on:click={closeAlert}
+              >
+                <span class="sr-only">Close</span>
+                <svg
+                  class="h-5 w-5"
+                  viewBox="0 0 20 20"
+                  fill="currentColor"
+                  aria-hidden="true"
+                >
+                  <path
+                    d="M6.28 5.22a.75.75 0 00-1.06 1.06L8.94 10l-3.72 3.72a.75.75 0 101.06 1.06L10 11.06l3.72 3.72a.75.75 0 101.06-1.06L11.06 10l3.72-3.72a.75.75 0 00-1.06-1.06L10 8.94 6.28 5.22z"
+                  />
+                </svg>
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    {/if}
   </div>
 </div>
